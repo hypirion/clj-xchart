b2c72e45a1c11f720edfe3da1cf5328bde4c93ca
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-06-19 20:21:26 +0200

    It's a start

--- src/com/hypirion/clj_xchart.clj
+++ src/com/hypirion/clj_xchart.clj
@@ -1,6 +1,36 @@
-(ns com.hypirion.clj-xchart)
+(ns com.hypirion.clj-xchart
+  (:import (org.knowm.xchart XYChart
+                             XChartPanel)
+           (javax.swing JPanel
+                        JFrame
+                        SwingUtilities)
+           java.awt.GridLayout))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn xy-chart
+  "Returns an xy-chart"
+  [{:keys [width height series]
+    :or {width 540 height 500}}]
+  {:pre [series]}
+  (let [chart (XYChart. width height)]
+    (doseq [[s-name [x-data y-data error-bars]] series]
+      (if error-bars
+        (.addSeries chart s-name x-data y-data error-bars)
+        (.addSeries chart s-name x-data y-data)))
+    chart))
+
+(defn view
+  "Utility function to render one or more charts in a swing frame."
+  [& charts]
+  (let [num-rows (int (+ (Math/sqrt (count charts)) 0.5))
+        num-cols (inc (/ (count charts)
+                         (double num-rows)))
+        frame (JFrame. "XChart")]
+    (SwingUtilities/invokeLater
+     #(do (.. frame (getContentPane) (setLayout (GridLayout. num-rows num-cols)))
+          (doseq [chart charts]
+            (if chart
+              (.add frame (XChartPanel. chart))
+              (.add frame (JPanel.))))
+          (.pack frame)
+          (.setVisible frame true)))
+    frame))
