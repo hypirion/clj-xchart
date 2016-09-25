8f3b957f8e599d999d64eb2d0e54bddf624cf644
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-09-25 15:11:22 +0200

    Back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.hypirion/clj-xchart "0.1.0"
+(defproject com.hypirion/clj-xchart "0.2.0-SNAPSHOT"
   :description "XChart wrapper for Clojure"
   :url "https://github.com/hyPiRion/clj-xchart"
   :license {:name "Eclipse Public License"
