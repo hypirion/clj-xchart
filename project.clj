eb9fd0ff69030632176ed8e22d65d2b52989e06b
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-10-14 00:03:14 +0200

    Add codox

--- project.clj
+++ project.clj
@@ -5,4 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.knowm.xchart/xchart "3.2.0"]]
+  :plugins [[lein-codox "0.10.1"]]
+  :deploy-repositories [["releases" :clojars]]
+  :codox {:source-uri "https://github.com/foo/bar/blob/{version}/{filepath}#L{line}"}
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
